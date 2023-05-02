a3e7b77ae074c97326f7ab9d0cbb17671e329fbb
santiago montanaro <santmon99@gmail.com>
2023-05-02 01:28:00 -0300

    chall finished

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    fontFamily: {
+      Kumbh: "Kumbh Sans, sans-serif;"
+    }
+  },
+  plugins: [],
+}
